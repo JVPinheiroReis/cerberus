344009e00c0d6e6e03fd26233b6ba2182ba64420
João Victor <joaovictorpinheiro1510@gmail.com>
2026-03-21 19:41:14 -0300

    feat(db): add PostgreSQL with docker compose and config a dumb client

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import app from "./app";
+import { pool } from "./db/client";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}...`);
-});
+async function start() {
+    try {
+        await pool.query("SELECT 1");
+        console.log("Database connected...");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error("Failed to start: ", err);
+        process.exit(1);
+    }
+}
+start();
